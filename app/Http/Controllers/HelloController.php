b3a984e6b6ea0ec936e08753682b4745cdfa36ae
toshi24128 <t_kwi@hotmail.com>
2021-10-14 01:37:50 +0900

    新規

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    //
+
+    public function index($id='noname',$pass='unknown'){
+        return <<<EOF
+        
+        <html>
+            <head>
+                <title>Hello/Index</title>
+                <style>
+                    body {font-size:16pt; color:#999}
+                    h1 {font-size:100px; text-aling:right; color:#eee; margin:-40px 0px -50px 0px;}
+                </style>
+            </head>
+            <body>
+                <h1>Index</h1>
+                <p>これは、Helloコントローラーのindexアクションです。</p>
+                <li>ID:{$id}</li>
+                <li>PASS:{$pass}</li>
+            </body>
+        </html>
+        EOF;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+/*
+Route::get('hello/{msg?}', function ($msg='no message.') {
+    $html = <<<EOF
+    <html><body><h1>hello</h1><p>This is sample page.{$msg}</p></body></html>
+    EOF;
+    return $html;
+});
+*/
+Route::get('/hello/{id?}/{pass?}','App\Http\Controllers\HelloController@index');
