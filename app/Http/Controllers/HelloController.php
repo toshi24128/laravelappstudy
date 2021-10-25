48fdeb98c2dd79044620cdd362f8ba1b3c0cc61a
toshi24128 <t_kwi@hotmail.com>
2021-10-26 02:43:36 +0900

    Bladeをつかったテンプレート

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(Request $request){
-        $date = ['msg'=>'これはコントローラから渡されたメッセージです。',
-            'id'=>$request->id
+    public function index(){
+        $date = ['msg'=>'これはBladeを利用したサンプルです。',
         ];
         
         return view('hello.index', $date);
