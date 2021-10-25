5897b1a13c6770c5710e4a1752f7aece5434a0fb
toshi24128 <t_kwi@hotmail.com>
2021-10-26 02:36:51 +0900

    ルートパラメーターをテンプレートにわたす

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,8 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(){
-        $date = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+    public function index($id = 'zero'){
+        $date = ['msg'=>'これはコントローラから渡されたメッセージです。',
+            'id'=>$id
+        ];
+        
         return view('hello.index', $date);
     }
 }
